e73d1579f54084f1974378d263315537e21b1b41
Nyovelt <Nyovelt@users.noreply.github.com>
2018-04-24 16:35:29 +0800

    First change

---
+++ Servo.ino
@@ -0,0 +1,17 @@
+#include "Servo.h"
+const int SERVO = 3;
+const int PIR_SENSOR = 6;
+Servo myservo;
+void setup() {
+        pinMode(PIR_SENSOR, INPUT);
+        myservo.attach(SERVO);
+        Serial.begin(9600);
+}
+void loop() {
+        myservo.write(90);
+        delay(2000);
+        myservo.write(0);
+        delay(2000);
+        
+
+}
